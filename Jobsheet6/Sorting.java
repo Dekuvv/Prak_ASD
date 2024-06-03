b2e14e4af783f493e9f01c8d08a9749254737430
Dekuvv <aditbagussadewa85@gmail.com>
2024-06-03 10:05:06 +0700

    new

--- Jobsheet6/Sorting.java
+++ Jobsheet6/Sorting.java
@@ -1,50 +1,6 @@
 package Jobsheet6;
 
 public class Sorting {
-    public static void sequentialSearch(int[] arr, int key){
-        System.out.println("======SEQUENTIAL SEARCH======");
-        for (int i = 0; i < arr.length; i++){
-            if ( arr[i] == key) {
-                System.out.println("Data ditemukan pada indeks ke-" + i);
-                return;
-            }   
-        }
-        System.out.println("Data tidak ditemukan");
-    }
-    public static int binarySearchAsc(int[] arr, int key){
-        int start = 0, end = arr.length - 1;
-        
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            
-            if (arr[mid] == key) {
-                return mid;
-            }
-            if (arr[mid] < key) {
-                start = mid + 1;
-            }
-            else{
-                end = mid -1;
-            }
-        }
-        return -1;
-    }
-    public static void main(String[] args) {
-        int [] daftarNilai = {10, 5, 20, 15, 80, 45};
-        sequentialSearch(daftarNilai, 15);
-
-        int [] sortedNilai = {5, 5, 10, 20, 30, 40, 50};
-        int index = binarySearchAsc(sortedNilai, 5);
-
-        if (index != -1) {
-            System.out.println("\n======BINARY SEARCH======");
-            System.out.println("Data ditemukan pada indeks ke-" + index);
-        }
-        else{
-            System.out.println("Data tidak ditemukan");
-        }
-    }
-}
 
     public static int sequentialSearch(int []arr, int key){
         int indeks = 0;
